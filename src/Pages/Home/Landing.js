d03c2b2fecd1ef7907db014c4d67c79228f59dfd
Alicia Rojo <aliciarojovera@hotmail.com>
2021-01-12 21:47:57 +0100

    landing

--- src/Pages/Home/Landing.js
+++ src/Pages/Home/Landing.js
@@ -4,26 +4,35 @@ import { Button } from '@material-ui/core'
 import EditIcon from '@material-ui/icons/Edit'
 
 const Landing = props => {
+    console.log(props)
     return (
-        <>             
-        <header className="hero" style={{backgroundImage:`url(${props.img})`}}>
-        <div className="content">
-            <p>{props.msg}</p>
-            <Link to={props.to} className="newInvLink">
-            <Button 
-                    variant="contained"
-                    color="secondary"
+        <>
+            <header className="hero" style={{ backgroundImage: `url(${props.img})` }}>
+                <div className="content">
+                    <p>{props.msg}</p>
+                    {props.github ?
+                        <> <a href="https://github.com/reneshibuya/filomenapp"><img className="github" src="https://github.githubassets.com/images/modules/logos_page/Octocat.png"></img></a><p className="small">Colabora en Github</p><br></br></>
+                        : ""}
+                    <br></br>
 
-                    startIcon={<EditIcon />}
+                    <Link to={props.to} className="newInvLink">
 
-                    >
-                    {props.btn}
-                </Button>
-            </Link>
-        </div>
-        </header>
+                        <Button
+                            variant="contained"
+                            color="secondary"
+
+                            startIcon={<EditIcon />}
+
+                        >
+                            {props.btn}
+
+                        </Button>
+
+                    </Link>
+                </div>
+            </header>
         </>
     );
 }
- 
+
 export default Landing;
--- src/Pages/Home/ThreeColumns.js
+++ src/Pages/Home/ThreeColumns.js
@@ -6,34 +6,31 @@ const ThreeColumns = () => {
         <>
 <div className="pricing-box-container">
   <div className="pricing-box text-center">
-    <p className="price">Fácil</p>
+    <p className="price">Accede al estado de las carreteras</p>
     <ul className="features-list">
-      <li><strong>Añade tus datos de facturación</strong></li>
-      <li><strong>Añade a tus clientes</strong></li>
-      <li><strong>Crea tus facturas</strong></li>
-      <li><strong>Editalas y descargalas</strong></li>
+      <li><strong>Accede cuándo y donde quieras</strong></li>
+      <li><strong>Revisa y actualiza los avisos de tu zona</strong></li>
+      <li><strong></strong></li>
     </ul>
     {/* <button className="btn-primary">Get Started</button> */}
   </div>
-  <div className="pricing-box pricing-box-bg-image text-center">
-    <p className="price">GRATIS</p>
+          <div className="pricing-box pricing-box-bg-image text-center" style={{ backgroundImage: `url("https://res.cloudinary.com/aliciarojo/image/upload/v1610482746/image_1_vwc2pl.png")` }}>
+    <p className="price">MAPA</p>
     <ul className="features-list">
-      <li><strong>Clientes ilimitados</strong></li>
-      <li><strong>Facturas ilimitadas</strong></li>
-      <li><strong>Sin planes de precios</strong></li>
+      <li><strong>Accede al mapa para ver las alertas de tu zona</strong></li>
+      
       <li><strong>Sin publicidad</strong></li>
     </ul>
     <Link to="/newinvoice">
-      <button className="btn-primary">empezar ahora</button>
+      <button className="btn-primary">ver mapa</button>
     </Link>
   </div>
   <div className="pricing-box text-center">
-    <p className="price">En la nube</p>
+    <p className="price">Tus datos están seguros</p>
     <ul className="features-list">
-      <li><strong>Accede cuándo y dónde quieras</strong></li>
-      <li><strong>Instala la app en tu móvil</strong></li>
-      <li><strong>Cambios en tiempo real</strong></li>
-      <li><strong>¿Necesitas algo más? </strong><Link className="cuentanos" to="/contactus"><strong>cuéntanoslo</strong></Link></li>
+      <li><strong>Nos comprometemos a no usar tus datos</strong></li>
+      <li><strong>y respetar tu privacidad</strong></li>
+     
     </ul>
   </div>
 </div>
